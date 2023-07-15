2f188551be154b66d5edda70621ee53b6b5f592e
coradini <gabe.cora1@hotmail.com>
2023-07-14 22:48:14 -0300

    muita coisa adicionada nem lembro mais

--- src/main/java/br/ufsm/csi/cursinho/controller/RemoverCursoController.java
+++ src/main/java/br/ufsm/csi/cursinho/controller/RemoverCursoController.java
@@ -1,23 +1,23 @@
-package br.ufsm.csi.cursinho.controller;
+    package br.ufsm.csi.cursinho.controller;
 
-import br.ufsm.csi.cursinho.DAO.CursoDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+    import br.ufsm.csi.cursinho.DAO.CursoDao;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
-public class RemoverCursoController {
+    @Controller
+    public class RemoverCursoController {
 
-    @Autowired
-    private CursoDao cursoDao;
+        @Autowired
+        private CursoDao cursoDao;
 
-    @GetMapping("/remover-curso/{id}")
-    public String removerCurso(@PathVariable("id") int id, RedirectAttributes redirectAttributes) {
-        cursoDao.deleteCurso(id);
-        redirectAttributes.addFlashAttribute("mensagem", "Curso removido com sucesso.");
-        return "redirect:/listarcursos";
+        @GetMapping("/remover-curso/{id}")
+        public String removerCurso(@PathVariable("id") int id, RedirectAttributes redirectAttributes) {
+            cursoDao.deleteCurso(id);
+            redirectAttributes.addFlashAttribute("mensagem", "Curso removido com sucesso.");
+            return "redirect:/listarcursos";
+        }
     }
-}
